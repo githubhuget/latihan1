a18753bdf43ff68702e8b134b66fe2139679cda7
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2023-11-24 13:04:48 +0700

    tugas task10.php

---
+++ hello.html/task10.php
@@ -0,0 +1,25 @@
+<?php
+function hitunggrade($nilai) {
+    if ($nilai > 0 && $nilai < 50) {
+        return 'E';
+    } elseif ($nilai >= 50 && $nilai < 60) {
+        return 'D';
+    }elseif ($nilai >= 60 && $nilai < 70) {
+        return 'C';
+    }elseif ($nilai >= 70 && $nilai < 80) {
+        return 'B';
+    }elseif ($nilai >= 80 && $nilai < 90) {
+        return 'A';
+    } elseif ($nilai >= 90) {
+        return 'A+';
+    } else {
+        return 'Nilai tidak valid';
+    }
+}
+$nilaiMahasiswa = 89;
+$grade = hitungGrade($nilaiMahasiswa);
+
+echo "Nilai Mahasiswa : $nilaiMahasiswa <br>";
+echo "Grade : $grade";
+
+?>
