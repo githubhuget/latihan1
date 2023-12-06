c6f0812d3efecd42010aee0310503a25c9fa741e
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2023-12-06 15:13:07 +0700

    task 3 dan 4

--- hello.html/date.php
+++ hello.html/date.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task 8
+    <title>task 3 dan 4
         <!-- 1000 hari ke depan dan 500 hari di masa lampau -->
     </title>
 </head>
@@ -62,20 +62,58 @@
     getFutureDates();
     getPastDates();
 </script> -->
+<!-- <?php
+// function perkalian($angka1, $angka2) {
+//     $hasil = $angka1 * $angka2;
+//     echo "Perkalian $angka1 x $angka2 adalah $hasil<br>";
+// }
+// $hasil = perkalian(20, 3);
+// echo "perkalian 20 x 3 adalah $hasil 60";
+// echo "<br>";
+
+// $perkalian = perkalian(9, 2);
+// echo"perkalian 9 x 2 adalah $perkalian 18";
+// echo "<br>";
+
+?> -->
+<h1> Tanggal untuk 1000 Hari ke Depan dan 500 Hari di Masa lampau</h1>
+
 <?php
-function perkalian($angka1, $angka2) {
-    $hasil = $angka1 * $angka2;
-    echo "Perkalian $angka1 x $angka2 adalah $hasil<br>";
+
+date_default_timezone_set('Asia/Jakarta');
+
+
+$currentDate = date('Y-m-d');
+
+
+$dateFuture = new DateTime($currentDate);
+$datePast = new DateTime($currentDate);
+
+
+echo '<h2>1000 Hari ke Depan</h2>';
+echo '<ul>';
+for ($i = 1; $i <= 1000; $i++) {
+    
+    $dateFuture->add(new DateInterval('P1D'));
+
+    
+    echo '<li>' . $dateFuture->format('l, d F Y') . '</li>';
 }
-$hasil = perkalian(20, 3);
-echo "perkalian 20 x 3 adalah $hasil 60";
-echo "<br>";
+echo '</ul>';
 
-$perkalian = perkalian(9, 2);
-echo"perkalian 9 x 2 adalah $perkalian 18";
-echo "<br>";
 
+echo '<h2>500 Hari di Masa Lalu</h2>';
+echo '<ul>';
+for ($i = 1; $i <= 500; $i++) {
+    
+    $datePast->sub(new DateInterval('P1D'));
+
+    
+    echo '<li>' . $datePast->format('l, d F Y') . '</li>';
+}
+echo '</ul>';
 ?>
+
 
     
 </body>
