5b6438e88eb5b4516c1a1732038c36dbec09d32d
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2023-12-06 12:59:20 +0700

    task 2

--- hello.html/date.php
+++ hello.html/date.php
@@ -1,16 +1,20 @@
+<?php
+ $todayDate = "06 des 2024";
+ $returnDate = date('d-M-Y', strtotime('+100 day',strtotime($todayDate)));
+     ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task 1</title>
+    <title>task 2</title>
 </head>
 
 <body>
     <h1>
-    <?php
-        echo date('l');
-        ?> </h1>
-
+    Tanggal hari ini = <?= $todayDate;  ?>
+    <br>
+    Tanggal +100 hari dari sekarang = <?= $returnDate;  ?>
+    </h1>
 </body>
 </html>
