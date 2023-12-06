0d019c463cf75a6c4eb8d67737ea7690c3d06522
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2023-12-06 13:42:54 +0700

    task 3

--- hello.html/date.php
+++ hello.html/date.php
@@ -1,20 +1,49 @@
-<?php
- $todayDate = "06 des 2024";
- $returnDate = date('d-M-Y', strtotime('+100 day',strtotime($todayDate)));
-     ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task 2</title>
+    <title>task 3
+        1000 hari ke depan
+    </title>
 </head>
 
 <body>
-    <h1>
-    Tanggal hari ini = <?= $todayDate;  ?>
-    <br>
-    Tanggal +100 hari dari sekarang = <?= $returnDate;  ?>
-    </h1>
+    <h1>daftar tanggal 1000 hari ke depan </h1>
+
+    <ul id="futureDateList"></ul>
+    <script>
+    function getFutureDates() {
+        var currentDate = new Date();
+        var futureDateList = document.getElementById("futureDateList");
+
+        for (var i = 1; i <= 1000; i++) {
+            
+            currentDate.setDate(currentDate.getDate() + 1);
+
+          
+            var listItem = document.createElement("li");
+            listItem.textContent = formatDate(currentDate);
+            futureDateList.appendChild(listItem);
+        }
+    }
+
+   
+    function formatDate(date) {
+        var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+        var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
+        var day = days[date.getDay()];
+        var dayOfMonth = date.getDate();
+        var month = months[date.getMonth()];
+        var year = date.getFullYear();
+
+        return day + ', ' + dayOfMonth + ' ' + month + ' ' + year;
+    }
+
+    
+    getFutureDates();
+
+    </script>
 </body>
 </html>
