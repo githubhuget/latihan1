1ca3e8153444577c0845a2c10fadd8994032c937
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2023-12-06 15:03:00 +0700

    task 8

--- hello.html/date.php
+++ hello.html/date.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>task 4
-        1000 hari ke depan dan 500 hari di masa lampau
+    <title>task 8
+        <!-- 1000 hari ke depan dan 500 hari di masa lampau -->
     </title>
 </head>
 
 <body>
-<h1>Daftar Tanggal untuk 1000 Hari ke Depan & 500 Hari di Masa lampau</h1>
+<!-- <h1>Daftar Tanggal untuk 1000 Hari ke Depan & 500 Hari di Masa lampau</h1>
 
 <h2>1000 Hari ke Depan</h2>
 <ul id="futureDateList"></ul>
@@ -61,7 +61,21 @@
 
     getFutureDates();
     getPastDates();
-</script>
+</script> -->
+<?php
+function perkalian($angka1, $angka2) {
+    $hasil = $angka1 * $angka2;
+    echo "Perkalian $angka1 x $angka2 adalah $hasil<br>";
+}
+$hasil = perkalian(20, 3);
+echo "perkalian 20 x 3 adalah $hasil 60";
+echo "<br>";
+
+$perkalian = perkalian(9, 2);
+echo"perkalian 9 x 2 adalah $perkalian 18";
+echo "<br>";
+
+?>
 
     
 </body>
---
+++ hello.html/latihan.php
@@ -0,0 +1,14 @@
+<?php
+function perkalian($angka1, $angka2) {
+    $hasil = $angka1 * $angka2;
+    echo "Perkalian $angka1 x $angka2 adalah $hasil<br>";
+}
+$hasil = perkalian(20, 3);
+echo "perkalian 20 x 3 adalah $hasil 60";
+echo "<br>";
+
+$perkalian = perkalian(9, 2);
+echo"perkalian 9 x 2 adalah $perkalian 18";
+echo "<br>";
+
+?>
